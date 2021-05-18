46b72f57c72fe92e259560ba1055ea5147fb7aee
M Kavya <73734974+MKAVYA01@users.noreply.github.com>
2021-05-18 22:54:53 +0530

    pattern

--- patterns.c
+++ patterns.c
@@ -5,7 +5,7 @@ int main(){
         while (num !=1 && num !=2){
         num = get_int("Enter type of pattern (1 or 2) ");
         }
-        int rows = get_int("Number of rows ");
+        int rows = get_int("Number of rows");
         if(num==1)
         {
             for(int i=0;i<rows;i++)
