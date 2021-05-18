d4418e61d5b25e52424bebcda07d014394aa438a
M Kavya <73734974+MKAVYA01@users.noreply.github.com>
2021-05-18 20:58:44 +0530

    patterns

--- patterns.c
+++ patterns.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 int main()
 {
-        num = get_int("Enter type of pattern (1 or 2) ");
+        int num = get_int("Enter type of pattern (1 or 2) ");
         int rows = get_int("Number of rows ");
         if(num == 1)
         {
